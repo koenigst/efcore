96c85cc6491b514b4b75ba6b812cf18bc9d80b1c
koenigst <koenigst@users.noreply.github.com>
2024-08-28 16:17:44 +0200

    wip

--- test/EFCore.FSharp.FunctionalTests/NorthwindFSharpSqlServerContext.fs
+++ test/EFCore.FSharp.FunctionalTests/NorthwindFSharpSqlServerContext.fs
@@ -3,14 +3,15 @@
 
 namespace Microsoft.EntityFrameworkCore.FSharp.FunctionalTests
 
+open System.ComponentModel.DataAnnotations
 open Microsoft.EntityFrameworkCore
 open Microsoft.EntityFrameworkCore.TestModels.Northwind
 
-[<CLIMutable>]
-type CustomerFSharp = {
-    CustomerID: string;
-    Address: string option;
-}    
+type CustomerFSharp() =
+    inherit Customer()
+
+    [<MaxLength(60)>]
+    member _.Address: string option = None
 
 type NorthwindFSharpSqlServerContext(options) =
     inherit NorthwindSqlServerContext(options)
@@ -18,4 +19,12 @@ type NorthwindFSharpSqlServerContext(options) =
     member self.CustomerFSharp = self.Set<CustomerFSharp>()
 
     override _.OnModelCreating builder =
-        do builder.Entity<CustomerFSharp>().ToTable("Customers") |> ignore
+        do base.OnModelCreating(builder)
+        do builder.Entity<CustomerFSharp>(fun b ->
+            do b.ToTable("Customers") |> ignore
+            //do b.HasKey(fun e -> e.CustomerID :> obj) |> ignore
+            //do b.Property(fun e -> e.CustomerID).HasColumnName("CustomerID") |> ignore
+            //do b.HasOne<Customer>().WithOne().HasForeignKey(fun e -> e.CustomerID :> obj).IsRequired() |> ignore
+            )
+            |> ignore
+        //do builder.Entity<CustomerFSharp>().HasKey(fun e -> e.CustomerID :> obj) |> ignore
